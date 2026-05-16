cb7c031242610ad9636e1b472a66768fdf43defb
kizza fredrich <82723282+kizzafredrichkibalama@users.noreply.github.com>
2026-05-16 23:54:28 +0300

    update

--- scripts/script.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.getElementById("menuBTN");
-const navMenu = document.getElementById("navMenu");
-
-menuBtn.addEventListener('click',()=>{
-    navMenu.classList.toggle('open');
-})
