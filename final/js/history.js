b147f877a057e405b3bb642511ea2746a3ab91bd
kizza fredrich <82723282+kizzafredrichkibalama@users.noreply.github.com>
2026-06-17 17:02:58 +0300

    updates

--- final/js/history.js
+++ final/js/history.js
@@ -1,3 +1,13 @@
 // History Page JavaScript
 
-initNav();
+// history.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  initNav();
+  setYear();
+});
+
+function setYear() {
+  const year = document.getElementById("year");
+  if (year) year.textContent = new Date().getFullYear();
+}
